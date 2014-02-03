62e4edbb40246e139a95d04af858a68870f2abab
jklotter <julie.klotter@gmail.com>
2014-02-03 17:37:33 -0600

    changed name

--- Do-It/AppDelegate.h
+++ Do-It/AppDelegate.h
@@ -3,7 +3,7 @@
 //  Do-It
 //
 //  Created by Dan Brajkovic on 1/27/14.
-//  Copyright (c) 2014 Dan Brajkovic. All rights reserved.
+//  Copyright (c) 2014 Julie Klotter. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
